26536dc6bbd7f253eab595f957641ec30092a98a
pratikguru <pratikgurudatt@pratiks-MacBook-Pro.local>
2020-05-03 19:05:28 +0530

    Build push

---
+++ build/precache-manifest.19bdda4575232a74adc6fccc5407fecf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b62636db5b087e7ab61ce0899690eaf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d1f2994d36a15af3e1b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "324227ffc23c01c6828e",
+    "url": "/static/js/2.ad965dca.chunk.js"
+  },
+  {
+    "revision": "d02058e580354236a30f577162253a2f",
+    "url": "/static/js/2.ad965dca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d1f2994d36a15af3e1b",
+    "url": "/static/js/main.fff7a0c3.chunk.js"
+  },
+  {
+    "revision": "afe77ddfd62062dbe992",
+    "url": "/static/js/runtime-main.4549e9b8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
